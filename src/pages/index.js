51bd564abf392c5c520dc26c487f9a53401b997b
Joe_01 <joe@noughtsandones.com>
2019-06-12 15:10:10 +0100

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import Nav from '../components/Nav';
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
@@ -8,13 +9,29 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div class="page">
+    <Nav />
+    <div class="index-wrapper">
+    <section>
+    </section>
+    <section>
+    </section>
+    <section>
+    </section>
+    <section>
+    </section>
+    <section>
+    </section>
+    <section>
+    </section>
+    <section>
+    </section>
+    <section>
+    </section>
+    <section>
+    </section>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    </div>
   </Layout>
 )
 
