98250620799b4c922958aec0efd21e0a417b777a
JoeCory94 <joecory94@gmail.com>
2019-06-12 16:11:45 +0100

    test

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { window } from 'browser-monads';
 import logo from '../../images/N&O_Logo_Dark_Blue_Full_Logo.png';
 import './nav.css';
 
@@ -9,12 +8,9 @@ const Nav = () => (
   <div className="nav__wrapper">
   <a className="nav__item--logo" href="/"><img src={ logo } className="logo"></img></a>
   <div className="nav__items">
-  <Link className={window.location.href.indexOf('work') > 0 ? 'nav__item--link active' : 'nav__item--link'} to='/work'>Work</Link>
-  <Link className={window.location.href.indexOf('about') > 0 ? 'nav__item--link active' : 'nav__item--link'} to='/about'>About</Link>
-  <Link className={window.location.href.indexOf('blog') > 0 ? 'nav__item--link active' : 'nav__item--link'} to='/blog'>Blog</Link>
-  <Link className={window.location.href.indexOf('contact') > 0 ? 'nav__item--link active' : 'nav__item--link'} to='/contact'>Contact</Link>
+
   </div>
-  <Link className={window.location.href.indexOf('contact') > 0 ? 'nav__item--button active' : 'nav__item--button nav__button'} to='/contact'>Contact</Link>
+  
   </div>
   </nav>
 )
