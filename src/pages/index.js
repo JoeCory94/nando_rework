a29b7c2a8030a4fce4583b538edb160c1941f565
JoeCory94 <joecory94@gmail.com>
2019-06-12 17:06:52 +0100

    f

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,9 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Nav from '../components/Nav';
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div class="page">
-    <Nav /> 
-    <div class="index-wrapper"> 
-    <section>
-    </section>
-    <section>
-    </section>
-    <section>
-    </section>
-    <section>
-    </section>
-    <section>
-    </section>
-    <section>
-    </section>
-    <section>
-    </section>
-    <section>
-    </section>
-    <section>
-    </section>
-    </div>
-    </div>
-  </Layout>
+  <div>
+  hello world
+  </div>
 )
 
 export default IndexPage
