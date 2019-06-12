a82af9b797935d53ac28fd1d0bbb44ec6b281c5b
JoeCory94 <joecory94@gmail.com>
2019-06-12 18:00:29 +0100

    f

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout";
-import Nav from '../components/Nav';
+import Nav from '../components/nav';
 
 const IndexPage = () => (
   <Layout>
